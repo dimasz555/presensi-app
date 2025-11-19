604ae8a82866af8b86a598c95075a1be30858a78
dimasz555 <h1101211047@student.untan.ac.id>
2025-11-19 21:06:50 +0700

    first commit

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->email()
+                    ->unique(ignoreRecord: true)
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                    ->dehydrated(fn(?string $state): bool => filled($state))
+                    ->required(fn(string $operation): bool => $operation === 'create'),
+                DateTimePicker::make('email_verified_at'),
+                Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple()
+                    ->preload()
+                    ->searchable(),
+            ]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-4xl font-bold items-center justify-center flex h-screen">
+        Hello world!
+    </h1>
+</body>
+
+</html>
